36b73f3945d865af5b2ec83018e6f4dbe0d50daa
Jason Costomiris <jcostom@jasons.org>
2023-04-14 23:25:57 -0400

    version bump

--- dme-update.py
+++ dme-update.py
@@ -35,7 +35,7 @@ HTTP_DATE_STRING = '%a, %d %b %Y %H:%M:%S GMT'
 # DME's record ID value.
 my_records = dict.fromkeys([record.strip() for record in RECORDS.split(',')], 'id')  # noqa E501
 
-VER = '1.10'
+VER = '1.11'
 USER_AGENT = f"dme-update.py/{VER}"
 
 # Cache Location
